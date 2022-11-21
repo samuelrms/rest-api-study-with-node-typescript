553a2a976666ec65452a525b741276024e661783
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2022-11-21 12:02:58 -0300

    Create migrations of city

---
+++ src/server/database/ETableNames.ts
@@ -0,0 +1,3 @@
+export enum ETableNames {
+  city = "city",
+}
---
+++ src/server/database/migrations/0000_create_city.ts
@@ -0,0 +1,22 @@
+import { Knex } from "knex";
+import { ETableNames } from "../ETableNames";
+
+export async function up(knex: Knex) {
+  return knex.schema
+    .createTable(ETableNames.city, (table) => {
+      table.bigIncrements("id").primary().index();
+      table.string("name", 150).index().notNullable();
+      table.string("country", 50).index().notNullable();
+
+      table.comment("Table used to store cities with system states.");
+    })
+    .then(() => {
+      console.log(`# Created table ${ETableNames.city}`);
+    });
+}
+
+export async function down(knex: Knex) {
+  return knex.schema.dropTable(ETableNames.city).then(() => {
+    console.log(`# Dropped table ${ETableNames.city}`);
+  });
+}
