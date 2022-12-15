686045e3286a9d8545c42861e5ed8b5495222d5b
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2022-12-15 17:04:52 -0300

    Fix controller cities deleteByID

--- src/server/database/providers/cities/index.ts
+++ src/server/database/providers/cities/index.ts
@@ -1,9 +1,13 @@
 import * as create from "./Create";
+import * as getAll from "./GetAll";
+import * as getByID from "./GetByID";
+import * as updateByID from "./UpdateByID";
+import * as deleteByID from "./DeleteByID";
 
 export const CitiesProvider = {
   ...create,
-  // ...getAll,
-  // ...getByID,
-  // ...updateByID,
-  // ...deleteByID,
+  ...getAll,
+  ...getByID,
+  ...updateByID,
+  ...deleteByID,
 };
