3552d22b6bfd0a81c2e6036e90655e4428589950
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2022-12-16 10:33:13 -0300

    fix: resolve production problem

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
+import { Knex } from "./server/database/knex";
 import { server } from "./server/Server";
 
-server.listen(process.env.PORT || 7777, () =>
+const startServer = () => {
+  server.listen(process.env.PORT || 7777, () =>
   console.log(`App rodando na porta ${process.env.PORT || 5555}`)
 );
+}
+
+if(process.env.IS_LOCALHOST !== "true") {
+  Knex.migrate.latest().then(() => {
+  startServer()
+}).catch(console.log);
+} else{
+  startServer();
+}
+
